45c74f21ff0601122ad4d931aa8732cd7d0a799a
Jensen Tonne <me@jstonne.com>
2012-02-02 15:23:14 +0800

    Added script name/path to module/url resolver.

--- source/jquery.require.script.js
+++ source/jquery.require.script.js
@@ -18,7 +18,32 @@ $.require.addLoader('script', (function()
 
 	var self = function() {
 
-		var batch = this;
+		var batch = this,
+			args = $.makeArray(arguments),
+			options,
+			names;
+
+		// Expand arguments into its actual definition
+		if ($.isPlainObject(args[0]) {
+			options = args[0];
+			names = args.slice(1);
+		} else {
+			names = args;
+		}
+
+		options = $.extend(self.defaultOptions, options, {batch: batch});
+
+		// Create tasks and add it to the batch.
+		var taskBefore;
+
+		$.each(names, function(i, name) {
+
+			var task = new self.task(name, options, taskBefore);
+
+			batch.addTask(task);
+
+			taskBefore = task;
+		});
 
 	};
 
@@ -43,6 +68,63 @@ $.require.addLoader('script', (function()
 		setup: function() {
 
 			$.extend(self.defaultOptions, options);
+		},
+
+		task: function(name, options, taskBefore)
+
+			var task = $.extend(this, $.Deferred());
+
+			task.name = name;
+
+			task.options = options;
+
+			// Module definition
+			if ($.isArray(name)) {
+
+				task.name = name.join('@');
+
+				task.moduleName = name = name[1];
+
+				// Set module flag
+				task.defineModule = true;
+
+				// Use XHR for module definitions
+				task.options.xhr = true;
+			}
+
+			// Resolve name to paths
+
+			// Absolute path
+			if ($.isUrl(name)) {
+
+				task.url = name;
+
+			// Relative path
+			} else if (/^(\/|\.)/.test(task.name)) {
+
+				task.url = $.uri(task.options.path)
+							.toPath(name)
+							.toString();
+
+			// Module path
+			} else {
+
+				task.url = $.uri(task.options.path)
+							.toPath('./' + name + task.options.extension)
+							.toString();
+
+				task.module = $.module(name);
+			}
+
+			task.start();
+		}
+
+	});
+
+	$.extend(self.task, {
+
+		start: function() {
+
 		}
 
 	});
